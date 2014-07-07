17386208694720bf91af1a3c0abcb46dfcbdd7d4
Zevran <info@zevran.net>
2014-07-07 20:22:29 +0200

    Rewrite all funcs - fixed all

--- cloudflare.go
+++ cloudflare.go
@@ -2,10 +2,8 @@ package cloudflare
 
 import (
 	"encoding/json"
-	"errors"
 	"io/ioutil"
 	"log"
-	"net"
 	"net/http"
 	"net/url"
 	"strings"
@@ -15,373 +13,434 @@ const (
 	CF_API_URL string = "https://www.cloudflare.com/api_json.html"
 )
 
-type CloudGuard struct {
+type Cloudflare struct {
 	ApiKey string
 	Email  string
 	Domain string
 	Debug  bool
 }
 
-func Connect(apikey, email, domain string, debug bool) *CloudGuard {
-	return &CloudGuard{
+func Connect(apikey, email string, debug bool) *Cloudflare {
+	return &Cloudflare{
 		ApiKey: apikey,
 		Email:  email,
-		Domain: domain,
 		Debug:  debug,
 	}
 }
 
-func (this *CloudGuard) SendRequest(values url.Values, structure interface{}) (*interface{}, error) {
+func (this *Cloudflare) SendRequest(values url.Values) ([]byte, error) {
 	values.Set("tkn", this.ApiKey)
 	values.Set("email", this.Email)
 
 	response, err := http.PostForm(CF_API_URL, values)
 	if err != nil {
 		log.Println(err)
-		return err
+		return nil, err
 	}
-	defer req.Body.Close()
+	defer response.Body.Close()
 
 	content, err := ioutil.ReadAll(response.Body)
 
 	if err != nil {
 		log.Println(err)
-	}
-
-	data := Root{}
-	data.Result = structure
-
-	err = json.Unmarshal(content, &data)
-
-	if err != nil {
 		return nil, err
 	}
-	return &data, nil
-
+	return content, nil
 }
 
-func (this *CloudGuard) GetDomainStats(interval string) (*Stats, error) {
-	values := url.Values
-	values.Set("z", this.Domain)
+func (this *Cloudflare) GetDomainStats(domain, interval string) (RootStats, error) {
+	values := url.Values{}
+	values.Set("z", domain)
 	values.Set("a", "stats")
 	values.Set("interval", interval)
 
-	data, err := this.SendRequest(values, Stats{})
+	response, err := this.SendRequest(values)
+
+	data := RootStats{}
+	err = json.Unmarshal(response, &data)
 	if err != nil {
-		return nil, err
+		return RootStats{}, err
 	}
-	return &data, nil
+	return data, nil
 }
 
-func (this *CloudGuard) GetDomainsList() (*ZonesLoad, error) {
-	values := url.Values
-	values.Set("z", this.Domain)
+func (this *Cloudflare) GetDomainsList() (RootZones, error) {
+	values := url.Values{}
 	values.Set("a", "zone_load_multi")
 
-	data, err := this.SendRequest(values, ZonesLoad{})
+	response, err := this.SendRequest(values)
+
+	data := RootZones{}
+	err = json.Unmarshal(response, &data)
 	if err != nil {
-		return nil, err
+		return RootZones{}, err
 	}
-	return &data, nil
+	return data, nil
 }
 
-func (this *CloudGuard) GetDnsRecords() (*DnsRecords, error) {
-	values := url.Values
-	values.Set("z", this.Domain)
+func (this *Cloudflare) GetDnsRecords(domain string) (RootDnsRecords, error) {
+	values := url.Values{}
+	values.Set("z", domain)
 	values.Set("a", "rec_load_all")
 
-	data, err := this.SendRequest(values, DnsRecords{})
+	response, err := this.SendRequest(values)
+
+	data := RootDnsRecords{}
+	err = json.Unmarshal(response, &data)
 	if err != nil {
-		return nil, err
+		return RootDnsRecords{}, err
 	}
-	return &data, nil
+	return data, nil
 }
 
-func (this *CloudGuard) NewDnsRecord(values ...map[string]string) (*Record, error) {
-	args := url.Values
-	args.Set("z", "rec_new")
-	for key, value := range values {
-		args.Set(key, value)
+func (this *Cloudflare) NewDnsRecord(domain string, values map[string]string) (RootNewRecord, error) {
+	args := url.Values{}
+	args.Set("z", domain)
+	args.Set("a", "rec_new")
+	for k, _ := range values {
+		args.Set(k, values[k])
 	}
-	data, err := this.SendRequest(args, Record{})
+
+	response, err := this.SendRequest(args)
+
+	data := RootNewRecord{}
+	err = json.Unmarshal(response, &data)
 	if err != nil {
-		return nil, err
+		return RootNewRecord{}, err
 	}
-	return &data, nil
+	return data, nil
 }
 
-func (this *CloudGuard) EditDnsRecord(id string, values ...map[string]string) (*Record, error) {
-	args := url.Values
-	args.Set("z", "rec_edit")
+func (this *Cloudflare) EditDnsRecord(domain, id string, values map[string]string) (RootEditRecord, error) {
+	args := url.Values{}
+	args.Set("z", domain)
+	args.Set("a", "rec_edit")
 	args.Set("id", id)
-	for key, value := range values {
-		args.Set(key, value)
+	for k, _ := range values {
+		args.Set(k, values[k])
 	}
-	data, err := this.SendRequest(args, Record{})
+
+	response, err := this.SendRequest(args)
+
+	data := RootEditRecord{}
+	err = json.Unmarshal(response, &data)
 	if err != nil {
-		return nil, err
+		return RootEditRecord{}, err
 	}
-	return &data, nil
+	return data, nil
 }
 
-func (this *CloudGuard) DeleteDnsRecord(id string) error {
-	values := url.Values
-	values.Set("z", this.Domain)
+func (this *Cloudflare) DeleteDnsRecord(domain, id string) (Root, error) {
+	values := url.Values{}
+	values.Set("z", domain)
 	values.Set("id", id)
-	data, err := this.SendRequest(values, interface{})
+
+	response, err := this.SendRequest(values)
+
+	data := Root{}
+	err = json.Unmarshal(response, &data)
 	if err != nil {
-		return nil, err
+		return Root{}, err
 	}
-	return &data, nil
+	return data, nil
 }
 
-func (this *CloudGuard) SetProxyStatus(id string, status bool) error {
+func (this *Cloudflare) SetProxyStatus(domain, id string, status bool) (RootEditRecord, error) {
 	proxy_status := "0"
 	if status {
 		proxy_status = "1"
 	}
-	records, err := this.GetDnsRecords()
-	if err != nil {
-		return err
-	}
-	for _, record := range records {
-		if record.Id == id {
-			values := make(map[string]string)
-			values["service_mode"] = proxy_status
-			_, err := this.EditDnsRecord(id, values)
-			if err != nil {
-				return err
-			}
-			return nil
-		}
-	}
-	return errors.New("Unknow dns record id")
+
+	values := make(map[string]string)
+	values["service_mode"] = proxy_status
+
+	response, err := this.EditDnsRecord(domain, id, values)
+
+	return response, err
 }
 
-func (this *CloudGuard) SetSecurityLevel(level string) (*ZoneLoad, error) {
-	values := url.Values
-	values.Set("z", this.Domain)
+func (this *Cloudflare) SetSecurityLevel(domain, level string) (RootZones, error) {
+	values := url.Values{}
+	values.Set("z", domain)
 	values.Set("a", "sec_lvl")
 	values.Set("v", level)
 
-	data, err := this.SendRequest(values, ZonesLoad{})
+	response, err := this.SendRequest(values)
+
+	data := RootZones{}
+	err = json.Unmarshal(response, &data)
 	if err != nil {
-		return nil, err
+		return RootZones{}, err
 	}
-	return &data, nil
+	return data, nil
 }
 
-func (this *CloudGuard) SetCacheLevel(level string) (*ZoneLoad, error) {
-	values := url.Values
-	values.Set("z", this.Domain)
+func (this *Cloudflare) SetCacheLevel(domain, level string) (RootZones, error) {
+	values := url.Values{}
+	values.Set("z", domain)
 	values.Set("a", "cache_lvl")
 	values.Set("v", level)
 
-	data, err := this.SendRequest(values, ZonesLoad{})
+	response, err := this.SendRequest(values)
+
+	data := RootZones{}
+	err = json.Unmarshal(response, &data)
 	if err != nil {
-		return nil, err
+		return RootZones{}, err
 	}
-	return &data, nil
+	return data, nil
 }
 
-func (this *CloudGuard) SetDevMode(enable bool) (*ZoneLoad, error) {
-	values := url.Values
-	values.Set("z", this.Domain)
+func (this *Cloudflare) SetDevMode(domain string, enable bool) (RootZones, error) {
+	dev := "0"
+	if enable {
+		dev = "1"
+	}
+
+	values := url.Values{}
+	values.Set("z", domain)
 	values.Set("a", "devmode")
-	values.Set("v", level)
+	values.Set("v", dev)
 
-	data, err := this.SendRequest(values, ZonesLoad{})
+	response, err := this.SendRequest(values)
+
+	data := RootZones{}
+	err = json.Unmarshal(response, &data)
 	if err != nil {
-		return nil, err
+		return RootZones{}, err
 	}
-	return &data, nil
+	return data, nil
 }
 
-func (this *CloudGuard) PurgeCache() (*PurgeCache, error) {
-	values := url.Values
-	values.Set("z", this.Domain)
+func (this *Cloudflare) PurgeCache(domain string) (RootPurgeCache, error) {
+	values := url.Values{}
+	values.Set("z", domain)
 	values.Set("a", "fpurge_ts")
 	values.Set("v", "1")
 
-	data, err := this.SendRequest(values, PurgeCache{})
+	response, err := this.SendRequest(values)
+
+	data := RootPurgeCache{}
+	err = json.Unmarshal(response, &data)
 	if err != nil {
-		return nil, err
+		return RootPurgeCache{}, err
 	}
-	return &data, nil
+	return data, nil
 }
 
-func (this *CloudGuard) PurgeFile(url string) (*PurgeFile, error) {
-	values := url.Values
-	values.Set("z", this.Domain)
-	values.Set("a", "fpurge_ts")
-	values.Set("url", url)
+func (this *Cloudflare) PurgeFile(domain, url_file string) (RootPurgeFile, error) {
+	values := url.Values{}
+	values.Set("z", domain)
+	values.Set("a", "zone_file_purge")
+	values.Set("url", url_file)
 
-	data, err := this.SendRequest(values, PurgeFile{})
+	response, err := this.SendRequest(values)
+
+	data := RootPurgeFile{}
+	err = json.Unmarshal(response, &data)
 	if err != nil {
-		return nil, err
+		return RootPurgeFile{}, err
 	}
-	return &data, nil
+	return data, nil
 }
 
-func (this *CloudGuard) LookupIp(ip string) error {
-	values := url.Values
-	values.Set("z", this.Domain)
+func (this *Cloudflare) LookupIp(domain, ip string) (RootLookupIp, error) {
+	values := url.Values{}
+	values.Set("z", domain)
 	values.Set("a", "ip_lkup")
 	values.Set("ip", ip)
 
-	_, err := this.SendRequest(values, interface{})
+	response, err := this.SendRequest(values)
+
+	data := RootLookupIp{}
+	err = json.Unmarshal(response, &data)
 	if err != nil {
-		return err
+		return RootLookupIp{}, err
 	}
-	return nil
+	return data, nil
 }
 
-func (this *CloudGuard) DenyIP(ip string) (*ModIp, error) {
-	values := url.Values
-	values.Set("z", this.Domain)
+func (this *Cloudflare) DenyIP(domain, ip string) (RootModIp, error) {
+	values := url.Values{}
+	values.Set("z", domain)
 	values.Set("a", "ban")
 	values.Set("key", ip)
 
-	data, err := this.SendRequest(values, ModIp{})
+	response, err := this.SendRequest(values)
+
+	data := RootModIp{}
+	err = json.Unmarshal(response, &data)
 	if err != nil {
-		return nil, err
+		return RootModIp{}, err
 	}
-	return &data, nil
+	return data, nil
 }
 
-func (this *CloudGuard) ForgetIP(ip string) (*ModIp, error) {
-	values := url.Values
-	values.Set("z", this.Domain)
+func (this *Cloudflare) ForgetIP(domain, ip string) (RootModIp, error) {
+	values := url.Values{}
+	values.Set("z", domain)
 	values.Set("a", "nul")
 	values.Set("key", ip)
 
-	data, err := this.SendRequest(values, ModIp{})
+	response, err := this.SendRequest(values)
+
+	data := RootModIp{}
+	err = json.Unmarshal(response, &data)
 	if err != nil {
-		return nil, err
+		return RootModIp{}, err
 	}
+	return data, nil
 }
 
-func (this *CloudGuard) AllowIP(ip string) (*ModIp, error) {
-	values := url.Values
-	values.Set("z", this.Domain)
+func (this *Cloudflare) AllowIP(domain, ip string) (RootModIp, error) {
+	values := url.Values{}
+	values.Set("z", domain)
 	values.Set("a", "wl")
 	values.Set("key", ip)
 
-	data, err := this.SendRequest(values, ModIp{})
+	response, err := this.SendRequest(values)
+
+	data := RootModIp{}
+	err = json.Unmarshal(response, &data)
 	if err != nil {
-		return nil, err
+		return RootModIp{}, err
 	}
+	return data, nil
 }
 
-func (this *CloudGuard) ToggleMirage2(toggle bool) error {
+func (this *Cloudflare) ToggleMirage2(domain string, toggle bool) (Root, error) {
 	status := "0"
 	if toggle {
 		status = "1"
 	}
-	values := url.Values
-	values.Set("z", this.Domain)
+	values := url.Values{}
+	values.Set("z", domain)
 	values.Set("a", "mirage2")
 	values.Set("v", status)
 
-	_, err := this.SendRequest(values, interface{})
+	response, err := this.SendRequest(values)
+
+	data := Root{}
+	err = json.Unmarshal(response, &data)
 	if err != nil {
-		return err
+		return Root{}, err
 	}
-	return nil
+	return data, nil
 }
 
-func (this *CloudGuard) Minify(state string) error {
-	values := url.Values
-	values.Set("z", this.Domain)
+func (this *Cloudflare) Minify(domain, state string) (Root, error) {
+	values := url.Values{}
+	values.Set("z", domain)
 	values.Set("a", "minify")
 	values.Set("v", state)
 
-	_, err := this.SendRequest(values, interface{})
+	response, err := this.SendRequest(values)
+
+	data := Root{}
+	err = json.Unmarshal(response, &data)
 	if err != nil {
-		return err
+		return Root{}, err
 	}
-	return nil
+	return data, nil
 }
 
-func (this *CloudGuard) SetRocketLoader(state string) error {
-	values := url.Values
-	values.Set("z", this.Domain)
+func (this *Cloudflare) SetRocketLoader(domain, state string) (Root, error) {
+	values := url.Values{}
+	values.Set("z", domain)
 	values.Set("a", "async")
 	values.Set("v", state)
 
-	_, err := this.SendRequest(values, interface{})
+	response, err := this.SendRequest(values)
+
+	data := Root{}
+	err = json.Unmarshal(response, &data)
 	if err != nil {
-		return err
+		return Root{}, err
 	}
-	return nil
+	return data, nil
 }
 
-func (this *CloudGuard) ToggleIpv46(toggle bool) error {
+func (this *Cloudflare) ToggleIpv46(domain string, toggle bool) (Root, error) {
 	status := "0"
 	if toggle {
 		status = "3"
 	}
-	values := url.Values
-	values.Set("z", this.Domain)
+	values := url.Values{}
+	values.Set("z", domain)
 	values.Set("a", "ipv46")
 	values.Set("v", status)
 
-	_, err := this.SendRequest(values, interface{})
+	response, err := this.SendRequest(values)
+
+	data := Root{}
+	err = json.Unmarshal(response, &data)
 	if err != nil {
-		return err
+		return Root{}, err
 	}
-	return nil
+	return data, nil
 }
 
-func (this *CloudGuard) Snapshot(zoneid string) error {
-	values := url.Values
-	values.Set("z", this.Domain)
+func (this *Cloudflare) Snapshot(domain, zoneid string) (Root, error) {
+	values := url.Values{}
+	values.Set("z", domain)
 	values.Set("a", "zone_grab")
 	values.Set("zid", zoneid)
 
-	_, err := this.SendRequest(values, interface{})
+	response, err := this.SendRequest(values)
+
+	data := Root{}
+	err = json.Unmarshal(response, &data)
 	if err != nil {
-		return err
+		return Root{}, err
 	}
-	return nil
+	return data, nil
 }
 
-func (this *CloudGuard) GetZoneSettings() (*ZoneSettings, error) {
-	values := url.Values
-	values.Set("z", this.Domain)
+func (this *Cloudflare) GetZoneSettings(domain string) (RootZoneSettings, error) {
+	values := url.Values{}
+	values.Set("z", domain)
 	values.Set("a", "zone_settings")
 
-	data, err := this.SendRequest(values, ZoneSettings{})
+	response, err := this.SendRequest(values)
+
+	data := RootZoneSettings{}
+	err = json.Unmarshal(response, &data)
 	if err != nil {
-		return nil, err
+		return RootZoneSettings{}, err
 	}
-	return &data, nil
+	return data, nil
 }
 
-func (this *CloudGuard) GetActiveZones(zones ...string) (*ZonesCheck, error) {
-	values := url.Values
-	values.Set("z", this.Domain)
+func (this *Cloudflare) GetActiveZones(domain string, zones ...string) (RootZonesCheck, error) {
+	values := url.Values{}
+	values.Set("z", domain)
 	values.Set("a", "zone_check")
 	values.Set("zones", strings.Join(zones, ","))
 
-	data, err := this.SendRequest(values, ZonesCheck{})
+	response, err := this.SendRequest(values)
+
+	data := RootZonesCheck{}
+	err = json.Unmarshal(response, &data)
 	if err != nil {
-		return nil, err
+		return RootZonesCheck{}, err
 	}
-	return &data, nil
+	return data, nil
 }
 
-func (this *CloudGuard) GetRecentIps(hours, class, geo string) (*ZoneIps, error) {
-	values := url.Values
-	values.Set("z", this.Domain)
+func (this *Cloudflare) GetRecentIps(domain, hours, class, geo string) (RootZoneIps, error) {
+	values := url.Values{}
+	values.Set("z", domain)
 	values.Set("a", "zone_ips")
 	values.Set("hours", hours)
 	values.Set("class", class)
 	values.Set("geo", geo)
 
-	data, err := this.SendRequest(values, ZoneIps{})
+	response, err := this.SendRequest(values)
+
+	data := RootZoneIps{}
+	err = json.Unmarshal(response, &data)
 	if err != nil {
-		return nil, err
+		return RootZoneIps{}, err
 	}
-	return &data, nil
+	return data, nil
 }
