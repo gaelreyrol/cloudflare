07568dac59df41847a371221b8907235aad25ec3
Zevran <info@zevran.net>
2014-07-08 10:48:28 +0200

    Rewrite sendrequest as private func

--- cloudflare.go
+++ cloudflare.go
@@ -28,7 +28,7 @@ func Connect(apikey, email string, debug bool) *Cloudflare {
 	}
 }
 
-func (this *Cloudflare) SendRequest(values url.Values) ([]byte, error) {
+func (this *Cloudflare) sendRequest(values url.Values) ([]byte, error) {
 	values.Set("tkn", this.ApiKey)
 	values.Set("email", this.Email)
 
@@ -54,7 +54,7 @@ func (this *Cloudflare) GetDomainStats(domain, interval string) (RootStats, erro
 	values.Set("a", "stats")
 	values.Set("interval", interval)
 
-	response, err := this.SendRequest(values)
+	response, err := this.sendRequest(values)
 
 	data := RootStats{}
 	err = json.Unmarshal(response, &data)
@@ -68,7 +68,7 @@ func (this *Cloudflare) GetDomainsList() (RootZones, error) {
 	values := url.Values{}
 	values.Set("a", "zone_load_multi")
 
-	response, err := this.SendRequest(values)
+	response, err := this.sendRequest(values)
 
 	data := RootZones{}
 	err = json.Unmarshal(response, &data)
@@ -83,7 +83,7 @@ func (this *Cloudflare) GetDnsRecords(domain string) (RootDnsRecords, error) {
 	values.Set("z", domain)
 	values.Set("a", "rec_load_all")
 
-	response, err := this.SendRequest(values)
+	response, err := this.sendRequest(values)
 
 	data := RootDnsRecords{}
 	err = json.Unmarshal(response, &data)
@@ -101,7 +101,7 @@ func (this *Cloudflare) NewDnsRecord(domain string, values map[string]string) (R
 		args.Set(k, values[k])
 	}
 
-	response, err := this.SendRequest(args)
+	response, err := this.sendRequest(args)
 
 	data := RootNewRecord{}
 	err = json.Unmarshal(response, &data)
@@ -120,7 +120,7 @@ func (this *Cloudflare) EditDnsRecord(domain, id string, values map[string]strin
 		args.Set(k, values[k])
 	}
 
-	response, err := this.SendRequest(args)
+	response, err := this.sendRequest(args)
 
 	data := RootEditRecord{}
 	err = json.Unmarshal(response, &data)
@@ -135,7 +135,7 @@ func (this *Cloudflare) DeleteDnsRecord(domain, id string) (Root, error) {
 	values.Set("z", domain)
 	values.Set("id", id)
 
-	response, err := this.SendRequest(values)
+	response, err := this.sendRequest(values)
 
 	data := Root{}
 	err = json.Unmarshal(response, &data)
@@ -165,7 +165,7 @@ func (this *Cloudflare) SetSecurityLevel(domain, level string) (RootZones, error
 	values.Set("a", "sec_lvl")
 	values.Set("v", level)
 
-	response, err := this.SendRequest(values)
+	response, err := this.sendRequest(values)
 
 	data := RootZones{}
 	err = json.Unmarshal(response, &data)
@@ -181,7 +181,7 @@ func (this *Cloudflare) SetCacheLevel(domain, level string) (RootZones, error) {
 	values.Set("a", "cache_lvl")
 	values.Set("v", level)
 
-	response, err := this.SendRequest(values)
+	response, err := this.sendRequest(values)
 
 	data := RootZones{}
 	err = json.Unmarshal(response, &data)
@@ -202,7 +202,7 @@ func (this *Cloudflare) SetDevMode(domain string, enable bool) (RootZones, error
 	values.Set("a", "devmode")
 	values.Set("v", dev)
 
-	response, err := this.SendRequest(values)
+	response, err := this.sendRequest(values)
 
 	data := RootZones{}
 	err = json.Unmarshal(response, &data)
@@ -218,7 +218,7 @@ func (this *Cloudflare) PurgeCache(domain string) (RootPurgeCache, error) {
 	values.Set("a", "fpurge_ts")
 	values.Set("v", "1")
 
-	response, err := this.SendRequest(values)
+	response, err := this.sendRequest(values)
 
 	data := RootPurgeCache{}
 	err = json.Unmarshal(response, &data)
@@ -234,7 +234,7 @@ func (this *Cloudflare) PurgeFile(domain, url_file string) (RootPurgeFile, error
 	values.Set("a", "zone_file_purge")
 	values.Set("url", url_file)
 
-	response, err := this.SendRequest(values)
+	response, err := this.sendRequest(values)
 
 	data := RootPurgeFile{}
 	err = json.Unmarshal(response, &data)
@@ -250,7 +250,7 @@ func (this *Cloudflare) LookupIp(domain, ip string) (RootLookupIp, error) {
 	values.Set("a", "ip_lkup")
 	values.Set("ip", ip)
 
-	response, err := this.SendRequest(values)
+	response, err := this.sendRequest(values)
 
 	data := RootLookupIp{}
 	err = json.Unmarshal(response, &data)
@@ -266,7 +266,7 @@ func (this *Cloudflare) DenyIP(domain, ip string) (RootModIp, error) {
 	values.Set("a", "ban")
 	values.Set("key", ip)
 
-	response, err := this.SendRequest(values)
+	response, err := this.sendRequest(values)
 
 	data := RootModIp{}
 	err = json.Unmarshal(response, &data)
@@ -282,7 +282,7 @@ func (this *Cloudflare) ForgetIP(domain, ip string) (RootModIp, error) {
 	values.Set("a", "nul")
 	values.Set("key", ip)
 
-	response, err := this.SendRequest(values)
+	response, err := this.sendRequest(values)
 
 	data := RootModIp{}
 	err = json.Unmarshal(response, &data)
@@ -298,7 +298,7 @@ func (this *Cloudflare) AllowIP(domain, ip string) (RootModIp, error) {
 	values.Set("a", "wl")
 	values.Set("key", ip)
 
-	response, err := this.SendRequest(values)
+	response, err := this.sendRequest(values)
 
 	data := RootModIp{}
 	err = json.Unmarshal(response, &data)
@@ -318,7 +318,7 @@ func (this *Cloudflare) ToggleMirage2(domain string, toggle bool) (Root, error) 
 	values.Set("a", "mirage2")
 	values.Set("v", status)
 
-	response, err := this.SendRequest(values)
+	response, err := this.sendRequest(values)
 
 	data := Root{}
 	err = json.Unmarshal(response, &data)
@@ -334,7 +334,7 @@ func (this *Cloudflare) Minify(domain, state string) (Root, error) {
 	values.Set("a", "minify")
 	values.Set("v", state)
 
-	response, err := this.SendRequest(values)
+	response, err := this.sendRequest(values)
 
 	data := Root{}
 	err = json.Unmarshal(response, &data)
@@ -350,7 +350,7 @@ func (this *Cloudflare) SetRocketLoader(domain, state string) (Root, error) {
 	values.Set("a", "async")
 	values.Set("v", state)
 
-	response, err := this.SendRequest(values)
+	response, err := this.sendRequest(values)
 
 	data := Root{}
 	err = json.Unmarshal(response, &data)
@@ -370,7 +370,7 @@ func (this *Cloudflare) ToggleIpv46(domain string, toggle bool) (Root, error) {
 	values.Set("a", "ipv46")
 	values.Set("v", status)
 
-	response, err := this.SendRequest(values)
+	response, err := this.sendRequest(values)
 
 	data := Root{}
 	err = json.Unmarshal(response, &data)
@@ -386,7 +386,7 @@ func (this *Cloudflare) Snapshot(domain, zoneid string) (Root, error) {
 	values.Set("a", "zone_grab")
 	values.Set("zid", zoneid)
 
-	response, err := this.SendRequest(values)
+	response, err := this.sendRequest(values)
 
 	data := Root{}
 	err = json.Unmarshal(response, &data)
@@ -401,7 +401,7 @@ func (this *Cloudflare) GetZoneSettings(domain string) (RootZoneSettings, error)
 	values.Set("z", domain)
 	values.Set("a", "zone_settings")
 
-	response, err := this.SendRequest(values)
+	response, err := this.sendRequest(values)
 
 	data := RootZoneSettings{}
 	err = json.Unmarshal(response, &data)
@@ -417,7 +417,7 @@ func (this *Cloudflare) GetActiveZones(domain string, zones ...string) (RootZone
 	values.Set("a", "zone_check")
 	values.Set("zones", strings.Join(zones, ","))
 
-	response, err := this.SendRequest(values)
+	response, err := this.sendRequest(values)
 
 	data := RootZonesCheck{}
 	err = json.Unmarshal(response, &data)
@@ -435,7 +435,7 @@ func (this *Cloudflare) GetRecentIps(domain, hours, class, geo string) (RootZone
 	values.Set("class", class)
 	values.Set("geo", geo)
 
-	response, err := this.SendRequest(values)
+	response, err := this.sendRequest(values)
 
 	data := RootZoneIps{}
 	err = json.Unmarshal(response, &data)
